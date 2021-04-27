52ab1c559ca2c375cf273ad557e692f4c0d9dcb5
Fábio Guimaraes <fabio_guimas@hotmail.com>
2021-04-27 15:15:28 -0300

    Acurácia 90% antes do cleanup

--- charGenerator.py
+++ charGenerator.py
@@ -2,77 +2,171 @@ from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 
-w_size, h_size = 100, 100
+import numpy as np
+
+#TODO: add numeros, caracteres especiais
+#TODO: canny nos dados
+#TODO: x == X
+
+wSize, hSize = 100, 100
+charSize = (32, 32)
 
 dir = 'data/train/'
 #dir = 'data/test/'
 # Treino
-fonts = ["arial.ttf", "consola.ttf", "AGENCYR.TTF", "ALGER.TTF", "ARLRDBD.TTF", "BROADW.TTF",
+# Futuramente: Edwardian Script ITC
+fonts = ["arial.ttf", "consola.ttf", "AGENCYR.TTF", "COLONNA.TTF", "ARLRDBD.TTF", "BROADW.TTF",
          "BRUSHSCI.TTF", "GOTHIC.TTF", "CHILLER.TTF", "comic.ttf", "COOPBL.TTF", "CURLZ___.TTF",
-         "GOUDYSTO.TTF"]
+         "JOKERMAN.TTF"]
 # Teste
 #fonts = ["YuGothL.ttc", "ntailu.ttf", "LATINWD.TTF", "CENTAUR.TTF"]
-# Futuramente: Edwardian Script ITC
 
-chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',\
-         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+# Considerações:
+# 'c' == 'C'
+# 'k' == 'K'
+# 'l' (L) ignorado por parecer com 'i' (I)
+# 'o' == 'O'
+# 'p' == 'P'
+# 's' == 'S'
+# 'u' == 'U'
+# 'v' == 'V'
+# 'w' === 'W'
+# 'x' == 'X'
+# 'z' == 'Z'
+chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',\
+         'A', 'B', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'L', 'M', 'N', 'Q', 'R', 'T', 'Y']
         # 'ç', 'Ç', 'á', 'à', 'é', 'ê', 'ó', 'ô', 'í', 'î', 'Á', 'À', 'É', 'Ê', 'Ó', 'Ô', 'Í', 'Î']
 
 # Modo da imagem
 # 1 -> pixel só ter valor de 0 ou 1
 # "L" -> pixel varia de 0 a 255
-modoImagem = "1"
+modoImagem = "L"
+
+# Margem na hora de recortar a imagem
+marginX = 10
+marginTop = 10
+marginBottom = 15
 
 n = 0
 for f in fonts:
 
     font = ImageFont.truetype(f, 50)
 
-    # Caractere normal
-    for c in chars:
-        img  = Image.new(mode = modoImagem, size = (w_size, h_size))
+    def imgCreate(c, crop = True):
+        img  = Image.new(mode = modoImagem, size = (wSize, hSize))
         draw = ImageDraw.Draw(img)
+
+        # Adiciona ruído à imagem
+        gauss = np.random.normal(10,5,wSize*hSize)
+        img.putdata(gauss)
+
+        # Adiciona ruído à posição
+        noiseX = np.random.randint(-5,5)
+        noiseY = np.random.randint(-5,5)
         
-        draw.text((w_size//2, h_size//2), c, 255, font=font, anchor="mm")
+        draw.text((wSize//2 + noiseX, hSize//2 + noiseY), c, 255, font=font, anchor="mm")
 
-        # Diferencia o nome se for minúsculo (l) ou maiúsculo (u)
-        name = c + "l" if c.islower() else c + "u"
-        name += str(n) + ".jpg"
+        # Recorta a região de interesse
+        if crop:
+            (w, h), _ = font.font.getsize(c)
 
-        img.save(dir + name)
-    
-    n += 1
+            top = hSize//2 - h//2 - marginTop
+            bottom = hSize//2 + h//2 + marginBottom
+            left = wSize//2 - w//2 - marginX
+            right = wSize//2 + w//2 + marginX
+            img = img.crop((left, top, right, bottom))
 
-    # Caractere rotacionado 15º
-    for c in chars:
-        img  = Image.new(mode = modoImagem, size = (w_size, h_size))
-        draw = ImageDraw.Draw(img)
+        # Ajusta o tamanho da imagem
+        img = img.resize(charSize)
+   
+
+        return img
+
+
+    def imgCreateBlank(strongNoise = False):
+        img  = Image.new(mode = modoImagem, size = charSize)
+
+        if strongNoise:
+            gauss = np.random.normal(127,50,charSize[0]*charSize[1])
+        else:
+            gauss = np.random.normal(20,10,charSize[0]*charSize[1])
+
+        img.putdata(gauss)
+
+        return img
+
+
+    def imgSave(c, img):
+        name = dir + c + str(n) + ".jpg"
+
+        img.save(name)
+
+
+    # Cria n imagens 'iguais' variando a posição
+    # e adicionando ruído
+    for _ in range(50):
+        # Caractere normal
+        for c in chars:
+            img = imgCreate(c)
+
+            imgSave(c, img)
         
-        draw.text((w_size//2, h_size//2), c, 255, font=font, anchor="mm")
+            n += 1
 
-        img = img.rotate(15,  expand=0)
+        # Caractere rotacionado 15º
+        for c in chars:
+            img = imgCreate(c)
 
-        # Diferencia o nome se for minúsculo (l) ou maiúsculo (u)
-        name = c + "l" if c.islower() else c + "u"
-        name += str(n) + ".jpg"
+            img = img.rotate(15)
 
-        img.save(dir + name) 
-    
-    n += 1
-
-    # Caractere rotacionado -15º
-    for c in chars:
-        img  = Image.new(mode = modoImagem, size = (w_size, h_size))
-        draw = ImageDraw.Draw(img)
+            imgSave(c, img)
         
-        draw.text((w_size//2, h_size//2), c, 255, font=font, anchor="mm")
+            n += 1
 
-        img = img.rotate(-15,  expand=0)
+        # Caractere rotacionado -15º
+        for c in chars:
+            img = imgCreate(c)
 
-        # Diferencia o nome se for minúsculo (l) ou maiúsculo (u)
-        name = c + "l" if c.islower() else c + "u"
-        name += str(n) + ".jpg"
+            img = img.rotate(-15)
 
-        img.save(dir + name) 
-    
-    n += 1
+            imgSave(c, img)
+
+            n += 1
+
+        # Caractere rotacionado 30º
+        for c in chars:
+            img = imgCreate(c)
+
+            img = img.rotate(30)
+
+            imgSave(c, img)
+        
+            n += 1
+
+        # Caractere rotacionado -30º
+        for c in chars:
+            img = imgCreate(c)
+
+            img = img.rotate(-30)
+
+            imgSave(c, img)
+
+            n += 1
+
+        # Imagens vazias
+        for _ in range(5):
+            img = imgCreateBlank()
+
+            imgSave('_', img)
+
+            n += 1
+
+        # Imagens com ruído forte
+        for _ in range(5):
+            img = imgCreateBlank(strongNoise=True)
+
+            imgSave('(', img)
+
+            n += 1
+
+        
--- dataHandler.py
+++ dataHandler.py
@@ -30,7 +30,11 @@ class dataHandler():
             
             self.train_features.append(feature)
             # Letras minúsculas == letras maísculas
-            self.train_labels.append(f[0].lower())
+            #self.train_labels.append(f[0].lower())
+            self.train_labels.append(f[0])
+
+        self.train_features = np.array(self.train_features)
+        self.train_labels = np.array(self.train_labels)
 
         for f in os.listdir(dir + "test/"):
             img = Image.open(dir + "test/" + f)
@@ -38,47 +42,52 @@ class dataHandler():
             
             self.test_features.append(feature)
             # Letras minúsculas == letras maísculas
-            self.test_labels.append(f[0].lower())
+            #self.test_labels.append(f[0].lower())
+            self.test_labels.append(f[0])
+        
+        self.test_features = np.array(self.test_features)
+        self.test_labels = np.array(self.test_labels)
 
         # Embaralha os dados
-        emb = list(zip(self.train_features, self.train_labels))
+        idxEmb = np.arange(self.train_features.shape[0])
+
+        for _ in range(nShuffle):
+            np.random.shuffle(idxEmb)
+
+        self.train_features = self.train_features[idxEmb]
+        self.train_labels = self.train_labels[idxEmb]
+
+        idxEmb = np.arange(self.test_features.shape[0])
         
         for _ in range(nShuffle):
-            random.shuffle(emb)
+            np.random.shuffle(idxEmb)
 
-        self.train_features, self.train_labels = zip(*emb)
-
-        emb = list(zip(self.test_features, self.test_labels))
-        
-        for _ in range(nShuffle):
-            random.shuffle(emb)
-
-        self.test_features, self.test_labels = zip(*emb)        
-
+        self.test_features = self.test_features[idxEmb]
+        self.test_labels = self.test_labels[idxEmb]
 
     def oneHotEncode(self):
         ''' Codifica os labels usando One Hot Encoding
         '''
-        self.classes = list(set(self.train_labels))
+        self.classes = np.unique(self.train_labels)
         newLabels = []
 
         for i in self.train_labels:
-            encoded = [0 for x in range(len(self.classes))]
-            index = self.classes.index(i)
-            encoded[index] = 1
+            encoded = np.zeros(self.classes.shape[0])
+            idx = np.where(self.classes == i)
+            encoded[idx] = 1
             newLabels.append(encoded)
             
-        self.train_labels = newLabels
+        self.train_labels = np.array(newLabels)
 
         newLabels = []
 
         for i in self.test_labels:
-            encoded = [0 for x in range(len(self.classes))]
-            index = self.classes.index(i)
-            encoded[index] = 1
+            encoded = np.zeros(self.classes.shape[0])
+            idx = np.where(self.classes == i)
+            encoded[idx] = 1
             newLabels.append(encoded)
             
-        self.test_labels = newLabels
+        self.test_labels = np.array(newLabels)
 
     
     def oneHotDecode(self, label):
@@ -89,6 +98,13 @@ class dataHandler():
 
         return self.classes[index]
 
+
+    def reshapeInputs(self):
+        ''' Ajusta o formato das features para trabalhar
+            com uma CNN
+        '''
+        self.train_features = self.train_features.reshape(-1,32,32,1)
+        self.test_features = self.test_features.reshape(-1,32,32,1)
 
     def getFeatures(self):
         '''Retorna as features divididas em treino e teste
@@ -105,5 +121,4 @@ class dataHandler():
     def getClassCount(self):
         '''Retorna a quantidade de classes diferentes
         '''
-        return len(self.classes)
-
+        return self.classes.shape[0]
