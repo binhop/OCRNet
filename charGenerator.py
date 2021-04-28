c47c13f4aa2549a88a28dc733f6fd55c140258c7
Fábio Guimaraes <fabio_guimas@hotmail.com>
2021-04-28 14:56:13 -0300

    Geração de dados e treino

--- charGenerator.py
+++ charGenerator.py
@@ -4,22 +4,30 @@ from PIL import ImageDraw
 
 import numpy as np
 
+import sys
+
 #TODO: add numeros, caracteres especiais
 #TODO: canny nos dados
-#TODO: x == X
+#TODO: blur nos dados
+#TODO: verificar 's'
+#TODO: remover g de uma das fontes
+#TODO: letra t sendo cortada no topo (corrigir obtenção do tamanho da fonte)
+#TODO: obter +1 fonte linha grossa
+#TODO: 1 T parece uma foice
 
 wSize, hSize = 100, 100
 charSize = (32, 32)
 
 dir = 'data/train/'
 #dir = 'data/test/'
+
 # Treino
 # Futuramente: Edwardian Script ITC
-fonts = ["arial.ttf", "consola.ttf", "AGENCYR.TTF", "COLONNA.TTF", "ARLRDBD.TTF", "BROADW.TTF",
-         "BRUSHSCI.TTF", "GOTHIC.TTF", "CHILLER.TTF", "comic.ttf", "COOPBL.TTF", "CURLZ___.TTF",
-         "JOKERMAN.TTF"]
+FONTS = ("arial.ttf", "consola.ttf", "AGENCYR.TTF", "COLONNA.TTF", "ARLRDBD.TTF", "FRAHV.TTF",
+         "JUICE___.TTF", "GOTHIC.TTF", "CHILLER.TTF", "comic.ttf", "COOPBL.TTF", "CURLZ___.TTF",
+         "JOKERMAN.TTF")
 # Teste
-#fonts = ["YuGothL.ttc", "ntailu.ttf", "LATINWD.TTF", "CENTAUR.TTF"]
+#FONTS = ["YuGothL.ttc", "ntailu.ttf", "LATINWD.TTF", "CENTAUR.TTF"]
 
 # Considerações:
 # 'c' == 'C'
@@ -33,8 +41,8 @@ fonts = ["arial.ttf", "consola.ttf", "AGENCYR.TTF", "COLONNA.TTF", "ARLRDBD.TTF"
 # 'w' === 'W'
 # 'x' == 'X'
 # 'z' == 'Z'
-chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',\
-         'A', 'B', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'L', 'M', 'N', 'Q', 'R', 'T', 'Y']
+CHARS = ['a', 'b', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',\
+         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'L', 'M', 'N', 'Q', 'R', 'T', 'Y']
         # 'ç', 'Ç', 'á', 'à', 'é', 'ê', 'ó', 'ô', 'í', 'î', 'Á', 'À', 'É', 'Ê', 'Ó', 'Ô', 'Í', 'Î']
 
 # Modo da imagem
@@ -44,37 +52,54 @@ modoImagem = "L"
 
 # Margem na hora de recortar a imagem
 marginX = 10
-marginTop = 10
-marginBottom = 15
+marginTop = 5
+marginBottom = 5
+
+# Quantas vezes criar cada tipo de letra
+repeatType = 40
 
 n = 0
-for f in fonts:
+TOTALSIZE = len(FONTS)*repeatType
+for f in range(len(FONTS)):
 
-    font = ImageFont.truetype(f, 50)
+    font = ImageFont.truetype(FONTS[f], 50)
 
     def imgCreate(c, crop = True):
         img  = Image.new(mode = modoImagem, size = (wSize, hSize))
         draw = ImageDraw.Draw(img)
 
-        # Adiciona ruído à imagem
-        gauss = np.random.normal(10,5,wSize*hSize)
-        img.putdata(gauss)
-
         # Adiciona ruído à posição
         noiseX = np.random.randint(-5,5)
         noiseY = np.random.randint(-5,5)
+
+        # Adiciona ruído à imagem
+        gauss = np.random.normal(10,5,wSize*hSize) # Ruido branco
+        # Ruído "agressivo"
+        if noiseX > 3:
+            gauss += np.random.choice([0, 255], size=wSize*hSize, p=[0.6, 0.4])
+        elif noiseX > 0:
+            gauss += np.random.choice([0, 255], size=wSize*hSize, p=[0.9, 0.1])
+        img.putdata(gauss)
+
         
-        draw.text((wSize//2 + noiseX, hSize//2 + noiseY), c, 255, font=font, anchor="mm")
+        draw.text((wSize//2 + noiseX, hSize//2 + noiseY), c, 255, font=font, anchor="lt")
 
         # Recorta a região de interesse
         if crop:
             (w, h), _ = font.font.getsize(c)
 
-            top = hSize//2 - h//2 - marginTop
-            bottom = hSize//2 + h//2 + marginBottom
-            left = wSize//2 - w//2 - marginX
-            right = wSize//2 + w//2 + marginX
+            top = hSize//2 - marginTop
+            bottom = hSize//2 + h + marginBottom
+            left = wSize//2 - marginX
+            right = wSize//2 + w + marginX
+
+            # Desenha retangulos na borda para não criar vies
+            # Aplica ~50% das vezes (usa o noiseX para determinar quando aplicar)
+            '''if noiseX < 0:
+                draw.rectangle([left+1, top+1, right-1, bottom-1], outline = 255, width = 5)'''
+
             img = img.crop((left, top, right, bottom))
+
 
         # Ajusta o tamanho da imagem
         img = img.resize(charSize)
@@ -104,9 +129,9 @@ for f in fonts:
 
     # Cria n imagens 'iguais' variando a posição
     # e adicionando ruído
-    for _ in range(50):
+    for r in range(repeatType):
         # Caractere normal
-        for c in chars:
+        for c in CHARS:
             img = imgCreate(c)
 
             imgSave(c, img)
@@ -114,7 +139,7 @@ for f in fonts:
             n += 1
 
         # Caractere rotacionado 15º
-        for c in chars:
+        for c in CHARS:
             img = imgCreate(c)
 
             img = img.rotate(15)
@@ -124,7 +149,7 @@ for f in fonts:
             n += 1
 
         # Caractere rotacionado -15º
-        for c in chars:
+        for c in CHARS:
             img = imgCreate(c)
 
             img = img.rotate(-15)
@@ -134,7 +159,7 @@ for f in fonts:
             n += 1
 
         # Caractere rotacionado 30º
-        for c in chars:
+        for c in CHARS:
             img = imgCreate(c)
 
             img = img.rotate(30)
@@ -144,7 +169,7 @@ for f in fonts:
             n += 1
 
         # Caractere rotacionado -30º
-        for c in chars:
+        for c in CHARS:
             img = imgCreate(c)
 
             img = img.rotate(-30)
@@ -169,4 +194,6 @@ for f in fonts:
 
             n += 1
 
-        
+        sys.stdout.write("\rConjunto de imagens processadas: %d/%d - %.1f%% - Imagens: %d"%(r + 1 + repeatType*f, TOTALSIZE, 100*(r + 1 + repeatType*f)/TOTALSIZE, n))
+        break
+    break
---
+++ fontViewer.py
@@ -0,0 +1,38 @@
+from PIL import Image, ImageFont, ImageDraw
+
+dir = 'data/'
+
+# Treino
+# Futuramente: Edwardian Script ITC
+FONTS = ("arial.ttf", "consola.ttf", "AGENCYR.TTF", "COLONNA.TTF", "ARLRDBD.TTF", "FRAHV.TTF",
+         "JUICE___.TTF", "GOTHIC.TTF", "CHILLER.TTF", "comic.ttf", "COOPBL.TTF", "CURLZ___.TTF",
+         "JOKERMAN.TTF")
+# Teste
+#fonts = ["YuGothL.ttc", "ntailu.ttf", "LATINWD.TTF", "CENTAUR.TTF"]
+
+PHRASES = ["abcdefghijklmnopqrstuvwxyz",
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+            "1234567890"]
+
+for f in FONTS:
+    font = ImageFont.truetype(f, 50)
+
+    w, h = (0,0)
+    for phr in PHRASES:
+        (w1, h1), _ = font.font.getsize(phr)
+        w = max(w1,w)
+        h += h1
+
+    img  = Image.new(mode = "L", size = (w, h+5*3))
+
+    draw = ImageDraw.Draw(img)
+
+    h = 5
+    for i in range(len(PHRASES)):
+        draw.text((0,h), PHRASES[i], 255, font=font, anchor="lt")
+        (_, h1), _ = font.font.getsize(PHRASES[i])
+        h += h1 + 5
+
+    name = dir + f.split(".")[0] + ".jpg"
+
+    img.save(name)
